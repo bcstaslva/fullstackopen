95caed291bafa42833012f6adea2769ff1425d7c
bcstaslva <cstaslva@gmail.com>
2025-05-28 02:00:06 -0300

    Resolução dos exercícios 1.1 e 1.2 da part1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+// Resolução dos exercício 1.1 e 1.2 da Part1
 
-function App() {
+const Header = () => {
+  const course = 'Desenvolvimento de Aplicação Half Stack';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{course}</h1>
     </div>
   );
-}
+};
+
+const Content = ({parts}) => {
+  return (
+    <div>
+      <p>{parts[0].part} {parts[0].exercise}</p>
+      <p>{parts[1].part} {parts[1].exercise}</p>
+      <p>{parts[2].part} {parts[2].exercise}</p>
+    </div>
+  );
+};
+
+const Total = ({parts}) => {
+  const total = parts.reduce((total, parts) => {
+    return total + parts.exercise;
+  }, 0);
+
+  return (
+    <div>
+      <p>Number of exercises {total}</p>
+    </div>
+  );
+};
+
+const App = () => {
+  const parts = [
+    {part: 'Fundamentos da biblioteca React', exercise: 10},
+    {part: 'Ussado props para passar dados', exercise: 7},
+    {part: 'Estado de um componente', exercise: 14}
+  ];
+
+  return (
+    <div>
+      <Header />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
 
 export default App;
